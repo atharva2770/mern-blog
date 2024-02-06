0255b2b2b5af0040b3c0a07712d5781a3bc63aee
atharva2770 <atharvapathak2770@gmail.com>
2024-02-06 15:59:04 +0530

    test api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// import userRoutes from './routes/user.route.js';
+import userRoutes from './routes/user.route.js';
 // import authRoutes from './routes/auth.route.js';
 // import postRoutes from './routes/post.route.js';
 // import commentRoutes from './routes/comment.route.js';
@@ -29,7 +29,7 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
 
-// app.use('/api/user', userRoutes);
+app.use('/api/user', userRoutes);
 // app.use('/api/auth', authRoutes);
 // app.use('/api/post', postRoutes);
 // app.use('/api/comment', commentRoutes);
